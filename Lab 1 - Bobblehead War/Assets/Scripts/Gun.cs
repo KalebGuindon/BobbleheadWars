33a606fb1965934ac81917acd74796ba6c5c7433
unknown <kmg0902@outlook.com>
2022-10-07 09:53:09 -0400

    Chapter 7 - Playing Sounds

--- Lab 1 - Bobblehead War/Assets/Scripts/Gun.cs
+++ Lab 1 - Bobblehead War/Assets/Scripts/Gun.cs
@@ -6,11 +6,12 @@ public class Gun : MonoBehaviour
 {
     public GameObject bulletPrefab;
     public Transform launchPosition;
+    private AudioSource audioSource;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        audioSource = GetComponent<AudioSource>();
     }
 
     
@@ -37,5 +38,6 @@ public class Gun : MonoBehaviour
         GameObject bullet = Instantiate(bulletPrefab) as GameObject;
         bullet.transform.position = launchPosition.position;
         bullet.GetComponent<Rigidbody>().velocity = transform.parent.forward * 100;
+        audioSource.PlayOneShot(SoundManager.Instance.gunFire);
     }
 }
